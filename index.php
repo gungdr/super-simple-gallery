67b77bda96c924828bc9b96a04e8b8fac8a37716
Agung Dwi Riansah <agungdwiriansah@gmail.com>
2017-04-04 21:19:46 +0700

    add jquery and event click on image

--- index.php
+++ index.php
@@ -1,8 +1,52 @@
-<?php
+<html>
+<head>
+	<title>Gallery</title>
+	<style>
+		img{
+			width: 100%;
+		}
+	</style>
+	<script
+	  src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+	  integrity="sha256-k2WSCIexGzOj3Euiig+TlR8gA0EmPjuc79OEeY5L45g="
+	  crossorigin="anonymous"></script>
+	</head>
+
+<body>
+	<?php
 	$dir = scandir("images");
+	$i = 0;
 	foreach ($dir as $file) {
 		if($file!='.' && $file!='..'){	
-			echo "<img src='images/".$file."' alt='".$file."' style='max-width:1024px;min-width:800px;' />";
+			if($i == 0){
+				echo "<img class='first-img' src='images/".$file."' alt='".$file."' />";
+				$i++;	
+			}else{
+				echo "<img data-src='images/".$file."' alt='".$file."' style='display:none;' />";
+			}
+			
 		}
 	}
-?>
+	?>
+
+<script>
+	$(function(){
+		var img = $('img');
+		img.on("click",function(){
+			var $this = $(this);
+			var next = $this.next();
+			var src = $this.prop('src');
+			$this.prop('src', '');
+			$this.prop('data-src', src);
+			$this.hide();
+			var data = next.data('src');
+			next.prop('src', data);
+			next.prop('data-src','');
+			next.show('fast');
+		});
+	});
+</script>	
+</body>
+
+
+</html>
